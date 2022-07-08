40361fd372260b262cb5055194f82b2c2bc5f8e9
Andreev Danila <mail@neandreev.ru>
2022-07-08 13:20:34 +0500

    add vercel edge deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/edge";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  adapter: vercel(),
 });
