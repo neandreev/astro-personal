e9dae03397ffd20dbe4c1c81923ebcf36a28f9c9
Danila Andreev <mail@neandreev.ru>
2022-11-14 15:05:02 +0500

    remove unused ssr code

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  // adapter: vercel(),
 });
