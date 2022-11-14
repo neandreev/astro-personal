5f191ee76a6ff68fca4184b1d8ac964cb44e007e
Danila Andreev <mail@neandreev.ru>
2022-11-14 15:03:14 +0500

    remove ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,5 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: vercel(),
+  // adapter: vercel(),
 });
