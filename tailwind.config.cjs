2b86bb93f48cb827b90f76840aa315cfdedc7338
Andreev Danila <mail@neandreev.ru>
2023-09-12 03:06:29 +0500

    switch fontsource Inter font to localhosted Roboto

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        sans: ["RobotoFlex", ...defaultTheme.fontFamily.sans],
+      },
+		},
 	},
 	plugins: [],
 }
